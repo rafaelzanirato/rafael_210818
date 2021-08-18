961ae43e0aac257f697bd3acf9f35aaa23cc2f8c
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-08-18 14:19:12 -0300

    Add files via upload

---
+++ rafael_210816.py
@@ -0,0 +1,22 @@
+print("Question 2 ***************************************************")
+joia=["terremoto","maremoto","furacão"]
+oil={"red":"vermelho","green":100,"gold":joia}
+print("Question 3 ***************************************************")
+print(oil)
+print("Question 4 ***************************************************")
+print(oil["red"])
+print("Question 5 ***************************************************")
+lup={"cake":"bolo"}
+oil["cold"]=lup
+print("Question 6 ***************************************************")
+print(oil)
+print("Question 7 ***************************************************")
+del oil["red"]
+print("Question 8 ***************************************************")
+print(oil)
+print("Question 9 e 10 ***************************************************")
+tapa={}
+tapa["limon"]="limão"
+tapa["piece"]="paz"
+tapa["war"]="afeganistão"
+print(tapa)
